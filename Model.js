3a8b6aa34290780a366ebfe7fa497cc341da64e7
andrew-s95 <56849472+andrew-s95@users.noreply.github.com>
2020-01-29 14:21:02 -0800

    Reworking file

--- Model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const BotSchema = new mongoose.Schema({
-    difficulty: {type: String, required: true},
-    algorithm: {type: String, required: true}
-}, {timestamps: true});
-
-module.exports = mongoose.model('algo', BotSchema);
