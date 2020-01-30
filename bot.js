5f2f5cf7c60ad58095b30f4602ff657c362a6d88
Kyung Jin Kang <kjinn62@gmail.com>
2020-01-30 09:27:36 -0800

    bug fixes

--- bot.js
+++ bot.js
@@ -4,21 +4,15 @@ const {
 	prefix,
 	token,
 } = require('./auth.json');
-const ytdl = require('ytdl-core');
 const fetch = require('node-fetch');
-const mongoose = require('mongoose');
-const ffmpeg = require('ffmpeg')
-
-
-// mongoose.connect("mongodb://localhost/Algo_Bot_algosAPI");
 
 // Initialize Discord Bot
 const bot = new Discord.Client();
 bot.login(auth.token)
 
 var algoProblem;
-const queue = new Map();
 
+//initiallizes bot and fetches api data
 bot.on('ready', () => {
     console.log("Connected");
     fetch("http://18.223.166.150/api/algos")
@@ -28,12 +22,11 @@ bot.on('ready', () => {
             })
 });
 
+//bot commands
 bot.on('message', async message => {
     if (message.author.bot) return;
-    if (message.content === '!ping') {
-        message.channel.send("pong");
-    }
-    if (message.content.startsWith === `${prefix}algo`) {
+    //command for returning random algos
+    if (message.content === `!algo`) {
         let num = Math.floor(Math.random() * Math.floor(algoProblem.length))
         message.channel.send("Difficulty: " + algoProblem[num].difficulty +  "\n Algorithm: " + algoProblem[num].algorithm)
     }
