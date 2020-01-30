535ee639da1c8f12f109609e9cf789faeb99fa9e
Jin Kang <kjinn62@gmail.com>
2020-01-29 16:14:24 -0800

    Algo API working

--- bot.js
+++ bot.js
@@ -2,22 +2,34 @@ const Discord = require('discord.js');
 const auth = require('./auth.json');
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
+const ffmpeg = require('ffmpeg')
 // mongoose.connect("mongodb://localhost/Algo_Bot_algosAPI");
 
 // Initialize Discord Bot
 const bot = new Discord.Client();
+bot.login(auth.token)
 
+var algoProblem;
 bot.on('ready', () => {
     console.log("Connected");
+    fetch("http://localhost:8000/api/algos")
+        .then(res => res.json())
+            .then(data => {
+                algoProblem = data.results;
+                console.log(algoProblem[0]);
+            })
 });
 
 bot.on('message', message => {
     if (message.content === '!ping') {
         message.channel.send("pong");
     }
+    if (message.content === '!algo') {
+        message.channel.send("Difficulty: " + algoProblem[0].difficulty +  "\n Algorithm: " + algoProblem[0].algorithm)
+    }
 });
 
-bot.login(auth.token)
+
 
 // fetch("https://leetcode.com/api/problems/algorithms/")
 //         .then(res => res.json())
