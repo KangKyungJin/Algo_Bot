c0e2803f558c55b12bc095acca8380fb9bf2106d
Kyung Jin Kang <kjinn62@gmail.com>
2020-01-31 10:35:58 -0800

    added the new commands to info command

--- commands/info.js
+++ commands/info.js
@@ -5,7 +5,12 @@ module.exports.run = async (bot, message, args, algoProblem, server, ago) => {
         "Hello, I'm Algo_Bot \n" +
         "Commands: \n" +
         "!info - command list \n" +
-        "!algos - gives a random algorithm"
+        "!algos - gives a random algorithm \n" +
+        "!play - plays the mp3 from a Youtube URL \n" +
+        "!skip - skips the current song \n" +
+        "!stop - stops the currently playing song and disconnects the bot \n" +
+        "!clear {{num}} - clears 'num' amounts of previous messages \n" +
+        "!embed - shows your info"
     );
 };
 
